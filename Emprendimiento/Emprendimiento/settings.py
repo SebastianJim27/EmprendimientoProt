402ed0a7330285a7be64e682ed9528ee4d2a4a20
SebastianJim27 <sebacho2004@gmail.com>
2025-04-09 10:34:39 -0500

    1
    la

--- Emprendimiento/Emprendimiento/settings.py
+++ Emprendimiento/Emprendimiento/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'foro'
+    'foro',
+    'registro',
+    'chatforo',
+    'infousuario',
 ]
 
 MIDDLEWARE = [
--- Emprendimiento/chatforo/views.py
+++ Emprendimiento/chatforo/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'foro/index.html')
+    return render(request, 'chatforo/chat.html')
